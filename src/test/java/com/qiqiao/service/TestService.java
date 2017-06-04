e7b1f80cd5252231069572b1ff1fa73e1db4c2e0
Magellans <244436991@qq.com>
2017-06-04 15:23:34 +0800

    add junit test

---
+++ src/test/java/com/qiqiao/service/TestService.java
@@ -0,0 +1,13 @@
+package com.qiqiao.service;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestService extends TestCase {
+
+	@Test
+	public void Testaa() {
+		
+	}
+}
